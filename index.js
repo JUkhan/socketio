288840e45fb3d172ec802ea464e66ef77f86475b
jukhan <jasim.uddin.khan@gmail.com>
2017-05-12 11:35:28 +0600

    intro

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 var _ = require('lodash-node');
 
 var users = [];
-
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res){
   res.sendfile('index.html');
 });
@@ -57,6 +57,9 @@ io.on('connection', function (socket) {
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
