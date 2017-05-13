e69dfc492955f33c01a5b1ffef74ed1d3cc3acca
jukhan <jasim.uddin.khan@gmail.com>
2017-05-13 13:07:22 +0600

    cross

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var _ = require('lodash-node');
 var port = process.env.PORT || 5000;
 var users = [];
 
+app.all('/', function(req, res, next) {  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
