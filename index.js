db49ae8973641442d4b8d0a6e754a2bbbab94309
jukhan <jasim.uddin.khan@gmail.com>
2017-05-13 13:23:12 +0600

    cors2

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var _ = require('lodash-node');
+var cors = require('cors');
 var port = process.env.PORT || 5000;
 var users = [];
 
-app.all('/', function(req, res, next) {  
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
+app.use(cors());
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
